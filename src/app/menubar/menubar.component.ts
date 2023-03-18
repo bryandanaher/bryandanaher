96e538d5df1aafbbec71ee2f5df7f03b5654cfcf
bryandanaher <bryandanaher@gmail.com>
2023-03-18 00:52:41 -0500

    Made the sidebars work, made portrait smaller

--- src/app/menubar/menubar.component.ts
+++ src/app/menubar/menubar.component.ts
@@ -7,10 +7,14 @@ import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./menubar.component.scss']
 })
 export class MenubarComponent implements OnInit {
-  faArrow = faArrowDown;
+  faBioArrow = faArrowDown;
+  faProjectsArrow = faArrowDown;
 
   bioExpanded: boolean = false;
+  projectsExpanded: boolean = false;
+
   bioBackground: string = "title-background";
+  projectsBackground: string = "title-background";
   iconContainer: string = "icon-container";
 
   constructor() { }
@@ -18,16 +22,30 @@ export class MenubarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleExpandProjects(): void {
+    if(this.projectsBackground === "title-background") {
+      this.projectsBackground = "title-background expanded";
+      this.iconContainer = "icon-container flex-container";
+      this.faProjectsArrow = faArrowUp;
+      this.projectsExpanded = true;
+    } else {
+      this.projectsBackground = "title-background";
+      this.iconContainer = "icon-container";
+      this.faProjectsArrow = faArrowDown;    
+      this.projectsExpanded = false;
+    }
+  }
+
   toggleExpandBio(): void {
     if(this.bioBackground === "title-background") {
       this.bioBackground = "title-background expanded";
       this.iconContainer = "icon-container flex-container";
-      this.faArrow = faArrowUp;
+      this.faBioArrow = faArrowUp;
       this.bioExpanded = true;
     } else {
       this.bioBackground = "title-background";
       this.iconContainer = "icon-container";
-      this.faArrow = faArrowDown;    
+      this.faBioArrow = faArrowDown;    
       this.bioExpanded = false;
     }
   }
